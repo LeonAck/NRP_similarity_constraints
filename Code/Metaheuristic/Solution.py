86097cccced4990d0b51e214bb837ef010b791f4
lback <l.b.ackerman@student.rug.nl/>
2022-03-04 15:27:35 +0100

    Move files

--- Code/Metaheuristic/Solution.py
+++ Code/Metaheuristic/Solution.py
@@ -4,5 +4,7 @@ class Solution:
     Class to store solutions of the problem
     """
 
-    def __init__(self, solution):
+    def __init__(self, initial_solution, solution):
+        self.solution =
         self.best_solution = solution
+
