289cb2fe0731bbfa02122ea09860b0e2cc9216e8
lback <l.b.ackerman@student.rug.nl/>
2022-05-07 16:36:13 +0200

    swap_operator af. Nog testen

--- Code/Metaheuristic/Invoke/Operators/swap_operator.py
+++ Code/Metaheuristic/Invoke/Operators/swap_operator.py
@@ -1,3 +1,5 @@
+from Invoke.Constraints.Rules.RuleH3 import RuleH3
+import random
 
 
 def change_operator(solution, scenario, k):
@@ -10,10 +12,128 @@ def change_operator(solution, scenario, k):
         change_information
     """
     # get a change that is allowed by hard constraints
-    change_info = get_feasible_change(solution, scenario)
+    change_info = get_feasible_swap(solution, scenario, k)
 
     # add penalty to objective
-    if change_info['feasible']:
-        change_info["cost_increment"], change_info['violation_increment'] = calc_new_costs_after_change(solution, scenario, change_info)
+    # if change_info['feasible']:
+    #     change_info["cost_increment"], change_info['violation_increment'] = calc_new_costs_after_swap(solution, scenario, change_info)
 
     return change_info
+
+def get_feasible_swap(solution, scenario, k):
+    """
+    Function to find a swap between to nurses that are skill-compatible and
+    is allowed by shift type successions
+    """
+    feasible = False
+    # create object to store information
+    swap_info = {}
+    infeasible_combinations = []
+    feasible_employees = list((scenario.employees._collection.keys()))
+    swap_info["employee_id"] = random.choice(feasible_employees)
+    while not feasible:
+        employee_id_1, employee_id_2, compatible = find_skill_compatible_employees(feasible_employees, scenario.employees, infeasible_combinations)
+
+        if compatible:
+            feasible_days = find_feasible_days_swap(solution, scenario, k, employee_id_1, employee_id_2)
+
+            if feasible_days:
+                swap_d_index = random.choice(feasible_days)
+                feasible = True
+
+        # remove infeasible combinations of nurses
+        infeasible_combinations.append([employee_id_1, employee_id_2])
+
+    if feasible:
+        # create information object
+        swap_info = create_swap_info(employee_id_1, employee_id_2, swap_d_index, k)
+        swap_info['feasible'] = True
+    else:
+        swap_info = {}
+        swap_info['feasible'] = False
+
+    return swap_info
+
+def find_feasible_days_swap(solution, scenario, k, employee_id_1, employee_id_2):
+    """
+    Find a swap that is allowed by the shift type successions
+    Start and end-point of stretch of nurse 1 can be entered in shift_assignment
+    of nurse 2, and vice versa.
+    :return:
+    start and end date of swap
+    """
+    # check what days are possible as a starting date for k number of days
+    feasible_days = list(range(0, scenario.num_days_in_horizon - k))
+
+    # for each day in range get assignmetns of nurse 1
+    # for each assignment of nurse 1, find the non-forbidden assignmetns of nurse 2
+    # this returns a list of feasible days
+    feasible_days = check_one_way_swap(solution, k, feasible_days, employee_id_1, employee_id_2)
+    feasible_days = check_one_way_swap(solution, k, feasible_days, employee_id_2, employee_id_1)
+
+    # for each day in feasible days get assignments of nurse 2
+    # for each assignments of nurse 2, check whether they match the assignments of nurse 1
+    # this should result into a list or None
+
+    return feasible_days
+
+def create_swap_info(employee_id_1, employee_id_2, start_index, k):
+    swap_info = {}
+
+    swap_info['employee_id_1'] = employee_id_1
+    swap_info['employee_id_2'] = employee_id_2
+    swap_info['start_index'] = start_index
+    swap_info['end_index'] = start_index - 1
+
+    return swap_info
+
+def check_one_way_swap(solution, k, feasible_days, employee_id_1, employee_id_2):
+    """
+    Function to check for list of feasible days whether a swap can be
+    made for a stretch starting at d_index in feasible days
+    """
+
+    for d_index in feasible_days:
+
+        if RuleH3().check_forbidden(solution.forbidden_shift_type_successions,
+                                            s_index_1=solution.shift_assignments[employee_id_2][d_index-1][0],
+                                            s_index_2=solution.shift_assignments[employee_id_1][d_index][0]) \
+            or RuleH3().check_forbidden(solution.forbidden_shift_type_successions,
+                                            s_index_1=solution.shift_assignments[employee_id_1][d_index+k-1][0],
+                                            s_index_2=solution.shift_assignments[employee_id_2][d_index+k][0]):
+            feasible_days.remove(d_index)
+
+    return feasible_days
+
+def find_skill_compatible_employees(feasible_employees, employee_collection, infeasible_combinations):
+    compatible = False
+    employee_id_1 = random.choice(feasible_employees)
+    skill_set_id = employee_collection._collection[employee_id_1].skill_set_id
+
+    employees_w_skill_set = [employee_id for employee_id in feasible_employees
+                             if employee_collection._collection[employee_id].skill_set_id == skill_set_id
+                             and employee_id != employee_id_1]
+
+    # remove combinations that have already been checked as infeasible
+    for combination in infeasible_combinations:
+        if employee_id_1 in combination:
+            combination.remove(employee_id_1)
+            employees_w_skill_set.remove(combination[0])
+    # check if nonempty
+    if employees_w_skill_set:
+        employee_id_2 = random.choice(employees_w_skill_set)
+        compatible = True
+    else:
+        employee_id_2 = None
+
+    return employee_id_1, employee_id_2, compatible
+
+def find_sets_of_skill_compatible_employees(employee_collection, scenario):
+    skill_set_indices = range(0, len(scenario.skill_sets))
+
+
+
+
+
+
+
